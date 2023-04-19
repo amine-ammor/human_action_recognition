f76dff1b42bc1387ba51f831270592a785cdf9a3
Amine AMMOR <aamineammor@gmail.com>
2023-04-19 19:38:25 +0000

    completed the training notebook and added a link to tensorboard

--- notebooks/training.py
+++ notebooks/training.py
@@ -1,66 +1,62 @@
+batch_size = 32
+lr = 10**(-4)
+n_epochs = 30
+fraction_splits = [0.6,0.4 ]
+
 # +
 # fill the cache and print informations about caching in other notebook
 from pathlib import Path
 from ploomber_engine.ipython import PloomberClient
 
-path_cache_setup = "./training_setup.ipynb"
-path_cache_setup = str(Path(path_cache_setup).resolve())
+path_training_setup = "./training_setup.py"
 
-client = PloomberClient.from_path(path_cache_setup,remove_tagged_cells="parameters")
-namespace = client.get_namespace(
-    dict(truncate = None,
-         location = '/root/.cache/keypoints',
-         clear_cache = False))
-# -
-video_dataloader = namespace["video_dataloader"]
-label_to_idx = namespace["label_to_idx"]
+import utils_notebook
+import psutil
+from tqdm import tqdm
+with utils_notebook.NotebookPyExecutor(path_training_setup,remove_tagged_cells="parameters",
+                                      truncate = None,location = '/root/.cache/keypoints',
+                                       clear_cache = False,batch_size=batch_size,
+                                       fraction_splits = fraction_splits,lr=lr) as nb_exec:
+    trainer = nb_exec.namespace["trainer"]
+    validator = nb_exec.namespace["validator"]
 
-nb_actions=len(label_to_idx)
+    train_video_dataloader = nb_exec.namespace["train_video_dataloader"]
+    val_video_dataloader = nb_exec.namespace["val_video_dataloader"]
+    print(nb_exec.path_training_setup_as_notebook)
+    assert Path(nb_exec.path_training_setup_as_notebook).exists()
 
-from action_transformer.models import action_transfomer_mod
-model = action_transfomer_mod.ActionTransformer(1,2,nb_actions=nb_actions)
 
 # +
-from torch import nn
+from torch.utils.tensorboard import SummaryWriter
 
-from torch import optim
-from torch.optim import lr_scheduler
-optimizer = optim.AdamW(model.parameters())
-scheduler = lr_scheduler.OneCycleLR(optimizer,max_lr=10**(-3),epochs=10,steps_per_epoch=len(video_dataloader))
+writer = SummaryWriter()
+print(writer.log_dir)
 # -
 
-for batch in video_dataloader:
-    out_model = model(*batch[:2])
-    break
-
-loss_fn = nn.CrossEntropyLoss()
-def loss_batch_fn(out_model,batch):
-    res = loss_fn(out_model,batch[-1])
-    return res
-
-
-out_model.shape,[el.shape for el in batch]
-
-import torchmetrics
-accuracy_metric = torchmetrics.Accuracy(task="multiclass",num_classes=model.nb_actions)
-metrics = {"accuracy":accuracy_metric}
-
 import torch
-batch[-1],out_model.argmax(1)
+trainer.model.load_state_dict(torch.load("tmp_model.pkl"))
 
+# +
+from tqdm import tqdm
 
-def extract_predictions(out_model,batch):
+from utils_notebook import print_memory
+
+for i,epoch in tqdm(enumerate(range(n_epochs))):
     
 
+    print_memory() # to check if there are no memory leak
 
-from action_transformer.model_handling import model_handler_mod
-model_handler = model_handler_mod.ModelHandler(model,optimizer,scheduler,
-                                              video_dataloader,loss_batch_fn=loss_batch_fn,
-                                              metrics=metrics,
-                                              extract_predictions=extract_predictions)
+    print("training")
+    train_metrics_vals = trainer.iterate_on_epoch()
+    print("validation")
+    val_metrics_vals = validator.iterate_on_epoch()
+    
+
+    writer.add_scalar("Loss/train",float(train_metrics_vals["loss"]),i)
+    writer.add_scalar("Accuracy/train",float(train_metrics_vals["accuracy"]),i)
+    
+    writer.add_scalar("Loss/val",float(val_metrics_vals["loss"]),i)
+    writer.add_scalar("Accuracy/val",float(val_metrics_vals["accuracy"]),i)
+# -
 
 
-for el in ["video_dataloader"]:
-    globals()[el] = namespace[el]
-
-batch,batch_frames_valide = next(iter(video_dataloader))
